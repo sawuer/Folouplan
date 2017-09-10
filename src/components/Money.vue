fa99143135d093500e8ddb94296341774c9f2427
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-09-10 22:38:20 +0600

    Some

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <h4 class=""><span class="light-text">All:</span> {{cashSum}} {{currentCurrency}}</h4>
+    <h5 class="text-xs-right"><b>{{cashSum}} {{currentCurrency}}</b></h5>
     <h5 class="light-text">Spending</h5>
-
 		<v-dialog v-model="dialog" persistent>
       <v-btn primary class="purple lighten-2" dark slot="activator">Add new</v-btn>
       <v-card>
@@ -14,7 +13,7 @@
 	          <v-text-field label="Purchase name" required hint="example of helper text only on focus"></v-text-field>
 	          <v-text-field label="Cost" required hint="example of helper text only on focus"></v-text-field>
 	          <v-select id="type" label="Type" required :items="['Food', 'Passage', 'Home']"></v-select>
-		        <v-menu lazy :close-on-content-click="false" v-model="menu" transition="scale-transition" offset-y full-width :nudge-left="40" max-width="290px">
+		        <v-menu lazy :close-on-content-click="false" v-model="date1" transition="scale-transition" offset-y full-width :nudge-left="40" max-width="290px">
 		          <v-text-field slot="activator" label="Picker in menu" v-model="e3" prepend-icon="event" readonly></v-text-field>
 		          <v-date-picker v-model="e3" no-title scrollable actions>
 		            <template scope="{ save, cancel }">
@@ -31,36 +30,64 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-
-sdfsd
 		<br>
-
 	  <v-data-table v-bind:headers="spendingHeader" :items="spendingItems">
 	    <template slot="items" scope="props">
-	      <td class="pur-name">
-	      	{{ props.item.name }}
-		      <v-btn class="completed-todos" @click="deletePurchase" icon>
+	      <td class="pur-date text-xs-left">
+	      	<v-btn class="completed-todos" @click="deletePurchase" icon>
 		        <v-icon class="grey--text">delete</v-icon>
 		      </v-btn>
+		      {{ props.item.date }}
 		    </td>
-	      <td class="pur-date text-xs-left">{{ props.item.date }}</td>
+	      <td class="pur-name">{{ props.item.name }}</td>
 	      <td class="pur-type text-xs-left">{{ props.item.type }}</td>
 	      <td class="pur-cost cost-td text-xs-right">{{ props.item.cost }} <span class="cost">{{currentCurrency}}</span></td>
 	    </template>
 	  </v-data-table>	
 
-		<br>
+
+
+
+
     <h5 class="light-text">Income</h5>
+    <v-dialog v-model="dialog2" persistent>
+      <v-btn primary class="purple lighten-2" dark slot="activator">Add new</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add new</span>
+        </v-card-title>
+        <v-card-text>
+        	<v-form v-model="valid" ref="form1">
+	          <v-text-field label="Income cash" required hint="example of helper text only on focus"></v-text-field>
+	          <v-select id="type" label="Type" required :items="['Work', 'Freelance']"></v-select>
+		        <v-menu lazy :close-on-content-click="false" v-model="date2" transition="scale-transition" offset-y full-width :nudge-left="40" max-width="290px">
+		          <v-text-field slot="activator" label="Picker in menu" v-model="e4" prepend-icon="event" readonly></v-text-field>
+		          <v-date-picker v-model="e4" no-title scrollable actions>
+		            <template scope="{ save, cancel }">
+		              <v-card-actions>
+		                <v-btn flat primary @click.native="cancel()">Cancel</v-btn>
+		                <v-btn flat primary @click.native="save()">Save</v-btn>
+		              </v-card-actions>
+		            </template>
+		          </v-date-picker>
+		        </v-menu>
+	          <v-btn class="blue--text darken-1" flat @click.native="dialog2 = false">Close</v-btn>
+	          <v-btn class="blue--text darken-1" @click="addIncome" @click.native="dialog2 = false" flat>Save</v-btn>
+		      </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+		<br>
 	  <v-data-table v-bind:headers="incomeHeader" :items="incomeItems">
 	    <template slot="items" scope="props">
-	      <td class="pur-date text-xs-left">
-	        {{ props.item.date }}
-	        <v-btn class="completed-todos" @click="deletePurchase" icon>
+	      <td class="inc-date text-xs-left">
+	        <v-btn class="completed-todos" @click="deleteIncome" icon>
 		        <v-icon class="grey--text">delete</v-icon>
 		      </v-btn>
+	        {{ props.item.date }}
 	       </td>
-	      <td class="pur-type text-xs-left">{{ props.item.type }}</td>
-	      <td class="pur-cost cost-td text-xs-right">{{ props.item.income }} <span class="cost">{{currentCurrency}}</span></td>
+	      <td class="inc-type text-xs-left">{{ props.item.type }}</td>
+	      <td class="inc-income cost-td text-xs-right">{{ props.item.income }} <span class="cost">{{currentCurrency}}</span></td>
 	    </template>
 	  </v-data-table>
 
@@ -78,14 +105,18 @@ sdfsd
     data () {
       return {
         dialog: false,
+        dialog2: false,
         valid: false,
+        valid2: false,
         e3: null,
-        menu: false,
+        e4: null,
+        date1: false,
+        date2: false,
         cashSum: 50000,
         currentCurrency: 'тг',
         spendingHeader: [
-          { text: 'Item', align: 'left', sortable: false, value: 'name' },
-          { text: 'Date', align: 'left', sortable: true, aria_sort: 'descending', value: 'date' },
+          { text: 'Date', align: 'left', sortable: true, value: 'date' },
+          { text: 'Item', align: 'left', value: 'name' },
           { text: 'Type', align: 'left', value: 'type' },
           { text: 'Cost', align: 'right', value: 'cost' }
         ],
@@ -122,13 +153,27 @@ sdfsd
         var date = form.$el[3].value
         setTimeout(function () {
           self.spendingItems.unshift({
-            value: false,
             name: purchase,
             cost: cost,
             date: date,
             type: type
           })
-          self.cashSum -= cost
+          self.cashSum -= +cost
+        }, 600)
+      },
+      addIncome () {
+        var self = this
+        var form = this.$refs.form1
+        var income = form.$el[0].value
+        var type = form.$el[1].previousSibling.textContent
+        var date = form.$el[2].value
+        setTimeout(function () {
+          self.incomeItems.push({
+            type: type,
+            date: date,
+            income: income
+          })
+          self.cashSum += +income
         }, 600)
       },
       takeOf () {
@@ -146,14 +191,27 @@ sdfsd
       deletePurchase (e) {
         var itemsL = Object.keys(this.spendingItems).length
         var row = e.target.parentNode.parentNode.parentNode
-        var name = row.querySelector('.pur-name').innerText.replace('delete', '').trim()
+        var name = row.querySelector('.pur-name').innerText
         var type = row.querySelector('.pur-type').innerText
-        var date = row.querySelector('.pur-date').innerText
+        var date = row.querySelector('.pur-date').innerText.replace('delete', '').trim()
         var cost = row.querySelector('.pur-cost').innerText.replace(this.currentCurrency, '').trim()
         for (var i = 0; i < itemsL; i++) {
           if (this.spendingItems[i].name === name && this.spendingItems[i].type === type && this.spendingItems[i].date === date) {
             this.spendingItems.splice(i, 1)
             this.cashSum += +cost
+          }
+        }
+      },
+      deleteIncome (e) {
+        var itemsL = Object.keys(this.incomeItems).length
+        var row = e.target.parentNode.parentNode.parentNode
+        var date = row.querySelector('.inc-date').innerText.replace('delete', '').trim()
+        var type = row.querySelector('.inc-type').innerText
+        var income = row.querySelector('.inc-income').innerText.replace(this.currentCurrency, '').trim()
+        for (var i = 0; i < itemsL; i++) {
+          if (this.incomeItems[i].income === +income && this.incomeItems[i].type === type && this.incomeItems[i].date === date) {
+            this.incomeItems.splice(i, 1)
+            this.cashSum -= +income
           }
         }
       }
